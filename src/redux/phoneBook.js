15266b58b9234455991ebbbf6300755210386457
Darya-Viunyk <dar.viunyk@gmail.com>
2023-03-09 14:03:15 +0200

    Added task 8

--- src/redux/phoneBook.js
+++ src/redux/phoneBook.js
@@ -2,19 +2,18 @@ import { createSlice } from '@reduxjs/toolkit';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-// const initialState = {
-//   filter: '',
-//   items: [],
-// };
+const contactsState = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
 export const phoneBook = createSlice({
   name: 'contacts',
-  initialState: [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ],
+  initialState: contactsState,
+  filter: '',
+
   reducers: {
     setFilter: (state, action) => {
       state.filter = action.payload;
@@ -28,8 +27,9 @@ export const phoneBook = createSlice({
   },
 });
 const persistConfig = {
-  key: 'react-06-phonebook',
+  key: 'root',
   storage,
+  blaсklist: ['filter'],
 };
 
 export const persistedPhoneBookReduser = persistReducer(
